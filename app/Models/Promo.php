25a07ee4aa561c7bf4930d869f4f90771ce29070
Andri Sutanto <me@andrisutanto.com>
2023-07-08 07:40:45 +0800

    Make Model, Migration, and Seeder
    Make Model, Migration, and Seeder

---
+++ app/Models/Promo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'shop_id',
+        'old_price',
+        'new_price',
+        'description',
+    ];
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+}
