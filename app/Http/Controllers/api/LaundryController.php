27bce1dbcf940a6e5d5a3f9b9cb88c58e710de4a
Andri Sutanto <me@andrisutanto.com>
2023-07-12 19:49:40 +0800

    Create API To Read All Data
    Create API To Read All Data

---
+++ app/Http/Controllers/api/LaundryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Laundry;
+use Illuminate\Http\Request;
+
+class LaundryController extends Controller
+{
+    function readAll()
+    {
+        //$laundries = Laundry::all();
+        $laundries = Laundry::with('user','shop')->get();
+
+        return response()->json([
+            'data' => $laundries,
+        ], 200);
+    }
+}
