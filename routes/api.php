00448962620f055a77f00d31f0af43601ee88fa9
Andri Sutanto <me@andrisutanto.com>
2023-07-16 14:52:36 +0800

    Update for Backend API
    Update for Backend API

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,17 @@ Route::get('/laundry', [LaundryController::class, 'readAll']);
 Route::get('/user', [UserController::class, 'readAll']);
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+  // promo
+  Route::get('/promo/limit', [PromoController::class, 'readLimit']);
+
+  // shop
+  Route::get('/shop/recommendation/limit', [ShopController::class, 'readRecommendationLimit']);
+  Route::get('/shop/search/city/{name}', [ShopController::class, 'searchByCity']);
+
+  // laundry
+  Route::get('/laundry/user/{id}', [LaundryController::class, 'whereUserId']);
+  Route::post('/laundry/claim', [LaundryController::class, 'claim']);
+});
